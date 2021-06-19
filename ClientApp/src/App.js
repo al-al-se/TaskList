0b7f364189e4889a5034a01a301a14a8e7de4af7
al-al-se <al.al.se@yandex.ru>
2021-06-20 00:58:06 +0300

    start make reack ui

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -7,8 +7,12 @@ import { Counter } from './components/Counter';
 
 import './custom.css'
 
+import axios from 'axios';
+
 export default class App extends Component {
   static displayName = App.name;
+
+  
 
   render () {
     return (
