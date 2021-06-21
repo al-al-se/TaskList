c1a56a0f06b18d34f55562a351e4cc2fb2259a83
al-al-se <al.al.se@yandex.ru>
2021-06-22 01:03:15 +0300

    start rect coding...

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -9,18 +9,70 @@ import './custom.css'
 
 import axios from 'axios';
 
+import {useMemo,  useState, useEffect } from "react";
+
+import Table from "./components/Table";
+
+function func()
+{
+  
+}
+
 export default class App extends Component {
   static displayName = App.name;
 
-  
-
   render () {
+
+    const columns = useMemo(
+      () => [
+        {
+         
+          Header: "Tasks",
+          
+          columns: [
+            {
+              Header: "Id",
+              accessor:"task.Id",
+              show: false
+            },
+            {
+              Header: "Name",
+              accessor:"task.Name"
+            },
+            {
+              Header: "Begin",
+              accessor:"task.Begin"
+            },
+            {
+              Header: "End",
+              accessor:"task.End"
+            },
+            {
+              Header: "Status",
+              accessor:"task.Status"
+            }
+          ]
+        }
+      ]
+    );
+
+    const [data, setData] = useState([]);
+
+    // Using useEffect to call the API once mounted and set the data
+    useEffect(() => {
+      (async () => {
+        const result = await axios("https://localhost:5001/Task/GetAll");
+        setData(result.data);
+      })();
+    }, []);
+
     return (
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        <Table columns={columns} data={data} />
+      </Layout>    
     );
   }
 }
